ee1bc24df6daa1df98baf2cb0e2595e46c800bc9
Samrou Teouri <teourisamrou@gmail.com>
2024-04-25 11:13:13 +0000

    Add of crossorigin middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import { incidentRouter } from "./routes/incidentRoutes";
 import { authRoutes } from './routes/authRoutes';
+import cors from 'cors';
 
 export const app: Application = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 // Incident routes
