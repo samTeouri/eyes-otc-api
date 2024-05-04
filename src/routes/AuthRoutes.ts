38a0f2d2d89a2c43a27b135d7f5c20568957606a
Samrou Teouri <teourisamrou@gmail.com>
2024-05-04 00:46:15 +0000

    admin authentication

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { body, header } from "express-validator";
-import { login, register } from "../controllers/AuthController";
+import { citizenLogin, adminLogin, adminRegister, citizenRegister } from "../controllers/AuthController";
 import { User } from "../models/User";
 
 export const authRoutes = express.Router();
 
-// Register route
+// Ctizen Register route
 authRoutes.post('/register',
     [
         body('lastName').isString().notEmpty(),
@@ -24,14 +24,45 @@ authRoutes.post('/register',
             }
         }),
     ],
-    register,
+    citizenRegister,
 );
 
-// Login route
+// Admin Register route
+authRoutes.post('/admin/register',
+    [
+        body('lastName').isString().notEmpty(),
+        body('firstName').isString().notEmpty(),
+        body('email').isString().notEmpty().custom(async (value) => {
+            const user =  await User.findOne({ where: {email: value} });
+            if (user) {
+                return Promise.reject('Email already registered');
+            }
+        }),
+        body('password').isAlphanumeric().notEmpty().isLength({min: 8}),
+        body('phone').isNumeric().notEmpty().custom(async (value) => {
+            const user =  await User.findOne({ where: {phone: value} });
+            if (user) {
+                return Promise.reject('Phone already registered');
+            }
+        }),
+    ],
+    adminRegister,
+);
+
+// Citizen Login route
 authRoutes.post('/login',
     [
         body('identifier').notEmpty(),
         body('password').notEmpty().isString(),
     ],
-    login,
+    citizenLogin,
+);
+
+// Admin login route
+authRoutes.post('/admin/login/',
+    [
+        body('identifier').notEmpty(),
+        body('password').notEmpty().isString(),
+    ],
+    adminLogin,
 );
